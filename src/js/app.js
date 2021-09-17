bfc560249d763ce246be813b1e13aa69ab1fa41b
Daniel <daniel_sh.99@abv.bg>
2021-09-17 23:58:58 +0300

    Solution

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,5 @@
 import "../scss/app.scss";
+const R = require('ramda');
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
@@ -8,5 +9,11 @@ window.addEventListener("DOMContentLoaded", () => {
     { age: 23, class: "is-warning" },
     { job: "programmer", class: "is-danger" },
   ];
+  let getClasses = R.pluck('class', arrayToPluck);
   const articles = document.querySelectorAll("article");
-});
+
+    for (let i = 0; i < articles.length; i++) {
+        articles[i].classList.add(getClasses[i]);
+    }
+  });
+
